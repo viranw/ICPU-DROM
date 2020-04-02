ffa1c2492f7ec2f4519d50f84a49849b3ae3bd60
Viran Weerasekera <viran.w@icloud.com>
2020-04-03 10:51:30 +1100

    Wire up start page

--- qf_drom/urls.py
+++ qf_drom/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views as baseViews
 
 urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
     path('database/', admin.site.urls),
+
+    path('', baseViews.start, name='start')
 
 ]
