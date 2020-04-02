981173c1cf305fa56d60f370f5840c016b6e9da4
Viran Weerasekera <viran.w@icloud.com>
2020-04-03 09:26:07 +1100

    Wire up Postgres database

--- qf_drom/settings.py
+++ qf_drom/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'qf_drom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'qf_drom',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST':'localhost',
+        'PORT': '5432'
     }
 }
 
