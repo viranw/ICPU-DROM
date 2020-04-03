71b7c95d9971d88c470fb03117fcba551cdd8b0a
Viran Weerasekera <viran.w@icloud.com>
2020-04-03 11:05:15 +1100

    Update settings

--- qf_drom/settings.py
+++ qf_drom/settings.py
@@ -18,8 +18,8 @@ import dotenv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 dotenv_file = os.path.join(BASE_DIR, ".env")
- if os.path.isfile(dotenv_file):
-     dotenv.load_dotenv(dotenv_file)
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
